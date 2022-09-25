38369d7e5728992c1c7f48af626de1a550dd7e77
David <davidbenages02@gmail.com>
2022-09-25 20:35:53 +0200

    imGui? Error de linea 1

--- MistEngine/Application.cpp
+++ MistEngine/Application.cpp
@@ -4,11 +4,8 @@ Application::Application()
 {
 	window = new ModuleWindow(this);
 	input = new ModuleInput(this);
-	audio = new ModuleAudio(this, true);
-	scene_intro = new ModuleSceneIntro(this);
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
-	physics = new ModulePhysics3D(this);
 
 
 	// The order of calls is very important!
@@ -19,12 +16,6 @@ Application::Application()
 	AddModule(window);
 	AddModule(camera);
 	AddModule(input);
-	AddModule(audio);
-	AddModule(physics);
-	
-	// Scenes
-	AddModule(scene_intro);
-
 
 	// Renderer last!
 	AddModule(renderer3D);
--- MistEngine/Application.h
+++ MistEngine/Application.h
@@ -6,11 +6,8 @@
 #include "Module.h"
 #include "ModuleWindow.h"
 #include "ModuleInput.h"
-#include "ModuleAudio.h"
-#include "ModuleSceneIntro.h"
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
-#include "ModulePhysics3D.h"
 
 
 class Application
@@ -18,11 +15,8 @@ class Application
 public:
 	ModuleWindow* window;
 	ModuleInput* input;
-	ModuleAudio* audio;
-	ModuleSceneIntro* scene_intro;
 	ModuleRenderer3D* renderer3D;
 	ModuleCamera3D* camera;
-	ModulePhysics3D* physics;
 
 
 private:
